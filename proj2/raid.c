71448d690b4497a2ef8c12952ecc42fab103fdc1
josh <0x0f0f@pm.me>
2023-10-24 12:14:20 -0400

    proj2: more cleanup

--- proj2/raid.c
+++ proj2/raid.c
@@ -1,5 +1,5 @@
 /*
-raid.c: encode a file using Hamming(7, 4) and write it to 7 files (emulating RAID 2).
+raid.c: encode a file using Hamming(7, 4) and write it across 7 files (emulating RAID 2).
 
 Usage:
     ./raid -f filename (default: test.txt)
@@ -11,55 +11,51 @@ Usage:
 
 #define DEFAULT_IN "test.txt"
 
+void init_raid2(FILE *raid2[7], char basename[128]);
 unsigned char encode_nibble(unsigned char nibble); 
 
-void get_paths(int argc, char **argv, char *input_path);
+void get_arg_paths(int argc, char **argv, char *input_path);
 FILE *get_file(char path[], char mode[]);
 
+
 int main(int argc, char **argv) {
-    char ch;
-    char input_path[128] = { 0 };
-    char output_path[128] = { 0 };
+    char ch, input_path[128] = { 0 };
+    FILE *input, *raid2[7];
 
-    // an array of 7 file descriptors. each file represents one of 7 RAID 2 drives
-    FILE *raid2[7] = { 0 };
+    /* setup */
 
-    get_paths(argc, argv, input_path);
-    FILE *input = get_file(input_path, "r");
+    get_arg_paths(argc, argv, input_path);
 
-    // initialize RAID 2 files, <input>.part<0-6>
-    for (int i = 0; i < 7; i++) {
-        sprintf(output_path, "%s.part%d", input_path, i);
-        raid2[i] = get_file(output_path, "w");
-    }
+    input = get_file(input_path, "r");
+    init_raid2(raid2, input_path);
 
-    // contains the two nibbles of each character being read
-    unsigned char nibbles[2] = { 0 };
+    /* encoding */
 
-    // stores the buffer for each bit in the hamming code, i.e.
-    // [0] = p1, [1] = p2, [2] = d1, [3] = p3, [4] = d2, [5] = d3, [6] = d4
+    // buffer for each output file
     unsigned char buffers[7] = { 0 };
     int buffer_index = 0;
 
+    // buffer for each nibble of a byte
+    unsigned char nibbles[2] = { 0 };
+
+    // read a byte from input
     while ((ch = fgetc(input)) != EOF) {
         nibbles[0] = ch >> 4;
         nibbles[1] = ch & 15;
 
         // for each nibble (2 per byte)
         for (int n = 0; n < 2; n++) {
-            // write each bit of the encoded nibbles to its corresponding buffer
+            // write each bit of the encoded nibble to its corresponding file buffer
             for (int i = 0; i < 7; i++) {
                 buffers[i] |= (encode_nibble(nibbles[n]) >> (6 - i) & 1) << (7 - buffer_index);
             }
 
             buffer_index++;
 
-            // once buffers are full, write them to file
+            // if buffers are full, write them to file & reset them
             if (buffer_index == 8) {
                 for (int i = 0; i < 7; i++) {
                     putc(buffers[i], raid2[i]);
-
-                    // reset buffer
                     buffers[i] = 0;
                 }
 
@@ -91,6 +87,16 @@ unsigned char encode_nibble(unsigned char nibble) {
     return encoded_nibble;
 }
 
+// initialize an array of 7 files representing 7 RAID 2 drives
+void init_raid2(FILE *raid2[7], char basename[128]) {
+    char output_path[128] = { 0 };
+
+    for (int i = 0; i < 7; i++) {
+        sprintf(output_path, "%s.part%d", basename, i);
+        raid2[i] = get_file(output_path, "w");
+    }
+}
+
 // helper for accessing and validating files, exits on error
 FILE *get_file(char path[], char mode[]) {
     FILE *file = fopen(path, mode);
@@ -105,7 +111,7 @@ FILE *get_file(char path[], char mode[]) {
 
 // process the command line options (or fall back to default values):
 //      -f <path>: input file
-void get_paths(int argc, char **argv, char *input_path) {
+void get_arg_paths(int argc, char **argv, char *input_path) {
     int opt;
 
     // check if input/output paths are given
